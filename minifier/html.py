e2d485493f6260a167f6e43294992aa56b26be6f
Miles Barr <1018475+milesbarr@users.noreply.github.com>
2025-05-02 07:11:38 -0700

    Initial commit

---
+++ minifier/html.py
@@ -0,0 +1,30 @@
+import re
+
+from bs4 import BeautifulSoup, Comment
+
+from minifier.css import minify_css
+from minifier.json import minify_json
+
+
+def minify_html(html: str) -> str:
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Remove comments.
+    for comment in soup.find_all(string=lambda s: isinstance(s, Comment)):
+        comment.extract()
+
+    # Minify CSS within style tags.
+    for style in soup.find_all("style"):
+        style.string.replace_with(minify_css(style.string))
+
+    # Minify JSON-LD within script tags.
+    for script in soup.find_all("script", type="application/ld+json"):
+        script.string.replace_with(minify_json(script.string))
+
+    # Convert to a string.
+    html = soup.encode(formatter="html5").decode()
+
+    # Collapse whitespace.
+    html = re.sub(r"\s+", " ", html)
+
+    return html
---
+++ tests/test_html_minification.py
@@ -0,0 +1,20 @@
+import unittest
+
+from minifier import minify_html
+
+
+class TestHTMLMinification(unittest.TestCase):
+    def test_minify_simple_html(self):
+        original_html = (
+            "<html>   <head><title>Test</title>   </head>   <body> Hello world"
+            "! <br /></body></html>"
+        )
+        expected_html = (
+            "<html> <head><title>Test</title> </head> <body> Hello world! <br>"
+            "</body></html>"
+        )
+        self.assertEqual(minify_html(original_html), expected_html)
+
+
+if __name__ == "__main__":
+    unittest.main()
